cd47e1f2246c211e5a400f0c5b9dfaacddb71ac7
myroslav-kolomiiets <mir.kolomiets@gmail.com>
2024-02-15 22:46:27 +0200

    Add snapshot for index page basic test

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -11,3 +11,8 @@ describe('Home', () => {
     expect(heading).toBeInTheDocument()
   })
 })
+
+it('renders homepage unchanged', () => {
+  const { container } = render(<Home />)
+  expect(container).toMatchSnapshot()
+})
