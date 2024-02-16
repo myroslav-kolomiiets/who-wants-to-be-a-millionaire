5debab30265ff48fefebf433ae2567130e3ad297
myroslav-kolomiiets <mir.kolomiets@gmail.com>
2024-02-16 15:12:13 +0200

    Refactor homepage layout and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import classnames from 'classnames';
 import Control, {
   controlThemes,
   controlSizes,
@@ -13,44 +14,44 @@ export default function Home() {
   const formattedEarnedAmount = new Intl.NumberFormat().format(earnedAmount);
 
   return (
-    <main className="homepage">
+    <main className={classnames('homepage', { final: earned })}>
       <div className="homepage__image">
         <Image
           priority
           src="/images/logo.svg"
-          width={624}
-          height={367}
-          alt="logo"
+          sizes="50vw"
           style={{
-            objectFit: 'contain',
+            width: '100%',
+            height: 'auto',
           }}
+          width={500}
+          height={300}
+          alt="logo"
         />
       </div>
       <div className="homepage__link">
-        <div className="homepage__link-wrapper">
-          {earned ? (
-            <div>
-              <h2 className="homepage__link-h2">Total score:</h2>
-              <h3 className="homepage__link-h3">{`$${formattedEarnedAmount} earned`}</h3>
-            </div>
-          )
-            : (
-              <h1 className="homepage__link-h1">
-                Who wants to be
-                <br />
-                a millionaire?
-              </h1>
-            )}
+        {earned ? (
+          <>
+            <h2 className="homepage__link-h2">Total score:</h2>
+            <h3 className="homepage__link-h3">{`$${formattedEarnedAmount} earned`}</h3>
+          </>
+        )
+          : (
+            <h1 className="homepage__link-h1">
+              Who wants to be
+              <br />
+              a millionaire?
+            </h1>
+          )}
 
-          <div className="homepage__link-button">
-            <Control
-              theme={controlThemes.primary}
-              size={controlSizes.responsive}
-              text={earned ? 'Try again' : 'Start'}
-              href="/game"
-              isNavigation
-            />
-          </div>
+        <div className="homepage__link-button">
+          <Control
+            theme={controlThemes.primary}
+            size={controlSizes.responsive}
+            text={earned ? 'Try again' : 'Start'}
+            href="/game"
+            isNavigation
+          />
         </div>
       </div>
     </main>
