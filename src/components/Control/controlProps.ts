a454d9d3262a950a3664c4b489929592946e4642
myroslav-kolomiiets <mir.kolomiets@gmail.com>
2024-02-25 18:54:26 +0200

    Update devDependencies and linter rules

---
+++ src/components/Control/controlProps.ts
@@ -0,0 +1,15 @@
+export enum ControlThemes {
+  Hexagonal = 'hexagonal',
+  Primary = 'primary',
+}
+
+export enum ControlStates {
+  IsCorrect = 'isCorrect',
+  IsWrong = 'isWrong',
+}
+
+export enum ControlSizes {
+  Small = 'small',
+  Medium = 'medium',
+  Responsive = 'responsive',
+}
